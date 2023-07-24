db0f109ab8557ad0fcb24b637f8278eddb820ce8
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-07-23 21:23:38 -0700

    add base hitting data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
-async function getIdByName(name) {
+async function getById(id) {
     let res;
     try {
         const response = await fetch('../db/People.json')
         if (response.ok) {
             const arr = await response.json()
             arr.forEach(el => {
-                if (el.nameFirst + " " + el.nameLast === name) {
-                    res = el.playerID
+                if (el.playerID === id) {
+                    res = el;
                 }
             })
-            return res
+            return res;
         } else {
             throw response
         }
@@ -45,21 +45,27 @@ async function getHittingStats(id) {
     try {
         const response = await fetch('../db/Batting.json')
         if (response.ok) {
-            const arr = await response.json()
-            return arr.filter(el => el.playerID === id)
+            const arr = await response.json();
+            return arr.filter(el => el.playerID === id);
         } else {
-            throw response
+            throw response;
         }
 
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
-async function appendBatting(id) {
+
+async function appendBatting(id, num) {
     arr = await getTotalBatting(id);
-    document.querySelector('.stats-avg').textContent = calcAVG(arr[1], arr[0]).toFixed(3)
-    
+    document.querySelector(`.stats-${num}-avg`).textContent = calcAVG(arr[1], arr[0]).toFixed(3);
+    document.querySelector(`.stats-${num}-slg`).textContent = calcSLG(arr[1], arr[2], arr[3], arr[4], arr[0]).toFixed(3);
+    document.querySelector(`.stats-${num}-obp`).textContent = calcOBP(arr[1], arr[6], arr[8], arr[5], arr[0]).toFixed(3);
+    document.querySelector(`.stats-${num}-opb`).textContent = calcOPS(arr[1], arr[2], arr[3], arr[4], arr[0], arr[1], arr[6], arr[8], arr[5]).toFixed(3)
+    document.querySelector(`.stats-${num}-hr`).textContent = arr[4];
+    document.querySelector(`.stats-${num}-rbi`).textContent = arr[7];
+    document.querySelector(`.stats-${num}-ab`).textContent = arr[0];
 }
 
 
@@ -74,6 +80,7 @@ async function getTotalBatting(id) {
     let sf = 0;
     let bb = 0;
     let rbi = 0;
+    let hbp = 0;
     obj.forEach((i) => {
         ab += i.AB
         hits += i.H
@@ -83,14 +90,20 @@ async function getTotalBatting(id) {
         sf += i.SF
         bb += i.BB
         rbi += i.RBI
+        hbp += i.HBP;
     })
-    totals.push(ab, hits, doub, trip, hr, sf, bb, rbi);
+    totals.push(ab, hits, doub, trip, hr, sf, bb, rbi, hbp);
 
     return totals
     
 }
 
-appendBatting('troutmi01');
+async function appendDetails(id, num) {
+    
+}
+
+appendBatting('troutmi01', 1);
+appendBatting('ruthba01', 2);
 
 function calcAVG(hits, atBats) {
     return hits / atBats;
@@ -101,24 +114,12 @@ function calcSLG(sing, doub, trip, hr, atBats) {
 }
 
 function calcOBP(hits, walks, hbp, sacFly, atBats) {
-    return (hits + walks + hbp) / (ab + walks + hbp + sacFly)
+    return (hits + walks + hbp) / (atBats + walks + hbp + sacFly)
 }
 
 function calcOPS(sing, doub, trip, hr, atBats, hits, walks, hbp, sacFly) {
     return calcSLG(sing, doub, trip, hr, atBats) + calcOBP(hits, walks, hbp, sacFly, atBats);
 }
 
-// getIdByName(currentPlayer).then(id => {
-    //     getHittingStats(id).then(res => console.log(res))
-    // });
-    
-// getHittingStats('troutmi01').then(res => console.log(res));
-// fetch('../db/Batting.json')
-//     .then(res => res.json())
-//     .then(arr => {
-    //         arr.forEach(el => {
-        //             if(el.playerID === 'troutmi01') {
-            //                 console.log(el);
-            //             }
-            //         })
-            //     })
+
+// getById('troutmi01').then(res => console.log(res));
